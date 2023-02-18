7062d7ed9dc94773c8d3c82c7cc5a3651e5e91f1
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-02-18 20:28:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
-const App = () => {
-//code here
-  return (
-    <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
-    </div>
-  )
-}
+function App() {
+  const [name, setName] = useState('');
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    const textElement = document.getElementById('text');
+    textElement.innerHTML = name;
+    // Instead of using document.getElementById, we can use the `name` state value to render the text
+  };
+
+  return (
+    <div>
+      <input id="input" type="text" value={name} onChange={handleNameChange} />
+      <button id="button" onClick={handleButtonClick}>Click</button>
+      <p>Hello my name is <a id="text">____</a> and I study at Newton School</p>
+    </div>
+  );
+}
 
 export default App;
