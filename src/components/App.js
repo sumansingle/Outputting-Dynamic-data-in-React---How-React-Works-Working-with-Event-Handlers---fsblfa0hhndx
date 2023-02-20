d8873508fb876b9790e7d02bffd0363d723458ac
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-02-20 22:04:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "../styles/App.css";
-const[name,setName]=useState('');
-const [curr,setCurr]=useState('____');
-function nameChange(e){
-setName(e.target.value);
-}
-return (
-<div id="main">
-<input id='input' onChange={nameChange}></input>
-<button id='button' onClick={() => setCurr(name)}>Click</button>
-<p id='text'> Hello my name is {curr} and I study at Newton School</p>
-</div>
-)
-}
+const App = () => {
+  const [name, setName] = useState("_____");
+  const [input, setInput] = useState("");
+  const handleButtonClick = (e) => {
+    setName(input);
+  };
+  const handleNameChange = (e) => {
+    setInput(e.target.value);
+  };
+  return (
+    <div id="main">
+      <input id="input" onChange={handleNameChange}></input>
+      <button id="button" onClick={handleButtonClick}>
+        Click
+      </button>
+      <p id="text"> Hello my name is {name} and I study at Newton School</p>
+    </div>
+  );
+};
+
 export default App;
