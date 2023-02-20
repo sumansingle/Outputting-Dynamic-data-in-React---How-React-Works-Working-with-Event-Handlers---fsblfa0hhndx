5f13ed9afe9bc7596b76afd495e6f746f60932c9
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-02-20 21:54:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
-import React,{useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 function App() {
-  const [name, setName] = useState('');
-
+  const [name, setName] = useState("_____");
+  const [input, setInput] = useState("");
   const handleNameChange = (event) => {
-    setName(event.target.value);
+    setInput(event.target.value);
+    console.log(input, name);
   };
-
   const handleButtonClick = () => {
-    const textElement = document.getElementById('text');
-    textElement.innerHTML = "";
-    textElement.innerHTML = name;
+    console.log(input, name);
+    setName(input);
+    console.log(input, name);
     // Instead of using document.getElementById, we can use the `name` state value to render the text
   };
 
   return (
     <div>
-      <input id="input" type="text" value={name} onChange={handleNameChange} />
-      <button id="button" onClick={handleButtonClick}>Click</button>
-      <p>Hello my name is <a id="text">____</a> and I study at Newton School</p>
+      <input id="input" type="text" onChange={handleNameChange} />
+      <button id="button" onClick={handleButtonClick}>
+        Click
+      </button>
+      <p>Hello my name is {name} and I study at Newton School</p>
     </div>
   );
 }
